c9c430ca7c005cb807250a4be8e2d374d2fa01ae
vanag897 <vanag897@gmail.com>
2025-10-23 00:06:25 +0300

    first commit

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
 const title = document.querySelector('#title');
 const menuItems = document.querySelectorAll('.menu-item');
+console.log(title);
+console.log(menuItems);
+
 const actionButton = document.querySelector('.btn');
 const navList = document.querySelector('.menu');
+console.log(actionButton);
+console.log(navList);
 
 title.textContent = "Привіт, студенти!";
 title.style.color = "royalblue";
 title.style.textTransform = "uppercase";
 
-const secondItem = menuItems[1];
-secondItem.style.backgroundColor = "#fe8686ff";
+const secondItem = document.querySelectorAll('.menu-item')[1];
+secondItem.style.backgroundColor = "#ff9090ff";
 secondItem.style.fontSize = "18px";
 
 actionButton.classList.toggle('hidden');
+console.log(actionButton.classList);
 
 title.setAttribute('data-version', '1.0');
+console.log(title.dataset.version);
 
 const link = document.createElement('a');
 link.setAttribute('href', 'http://vcnuft.vn.ua/');
 link.textContent = 'Перейти на сайт коледжу';
-link.style.display = 'block';
-link.style.margin = '10px 0';
 document.body.append(link);
 
 const paragraph = document.createElement('p');
 paragraph.textContent = 'Цей елемент створено динамічно.';
 document.body.append(paragraph);
-
 setTimeout(() => {
     paragraph.remove();
 }, 5000);
 
 const footer = document.createElement('footer');
 footer.textContent = 'Розробник: Гілевич Іван';
-footer.style.marginTop = '20px';
-footer.style.cursor = 'pointer';
 document.body.append(footer);
-
 footer.addEventListener('dblclick', () => {
     footer.remove();
 });
 
 actionButton.innerHTML += ' <strong>Натисніть ТУТ</strong>';
+console.log(actionButton.innerHTML);
 
 title.insertAdjacentHTML("afterend", "<h2>Тема заняття: DOM</h2>");
 navList.insertAdjacentHTML("beforebegin", '<p style="font-style: italic;">Список навігації:</p>');
